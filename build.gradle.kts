5ea51463c654b6225e74c25bd81d1ab659e38210
RezMike <mihail.reznichenko.98@mail.ru>
2020-03-21 13:49:39 +0700

    Add korge config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,23 @@
 import com.soywiz.korge.gradle.*
 
 buildscript {
-	val korgePluginVersion: String by project
+    val korgePluginVersion: String by project
 
-	repositories {
-		mavenLocal()
-		maven { url = uri("https://dl.bintray.com/korlibs/korlibs") }
-		maven { url = uri("https://plugins.gradle.org/m2/") }
-		mavenCentral()
-	}
-	dependencies {
-		classpath("com.soywiz.korlibs.korge.plugins:korge-gradle-plugin:$korgePluginVersion")
-	}
+    repositories {
+        mavenLocal()
+        maven { url = uri("https://dl.bintray.com/korlibs/korlibs") }
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.soywiz.korlibs.korge.plugins:korge-gradle-plugin:$korgePluginVersion")
+    }
 }
 
 apply<KorgeGradlePlugin>()
 
 korge {
-	id = "com.sample.demo"
+    id = "io.github.rezmike.2048"
+    name = "2048"
+    icon = file("src/commonMain/resources/korge.png")
 }
